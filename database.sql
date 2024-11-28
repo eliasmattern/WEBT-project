206c63e3670e9d4611a3cb0ee5aed4adce99a41d
eliasmattern <eliasmattern02@gmail.com>
2024-11-28 13:30:34 +0100

    adds db

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS MoodTracker;
+
+USE MoodTracker;
+
+CREATE TABLE moods (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    mood ENUM('stressed', 'sad', 'angry', 'drained', 'overwhelmed', 'lonely',
+              'indifferent', 'content', 'peaceful',
+              'happy', 'joyful', 'grateful', 'calm', 'energized', 'excited', 'amazed') NOT NULL,
+    description VARCHAR(500) NULL,
+    mood_category ENUM('Unpleasant Mood', 'Neutral Mood', 'Positive Mood') NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE INDEX idx_date ON moods (date);

9fead9096c71eff653ac7cdb5d3b8be6f8785ae4
eliasmattern <coalcrafter202@gmail.com>
2025-01-03 13:25:21 +0100

    DONE 💀

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const valid_moods = ["stressed", "sad", "angry", "drained", "overwhelmed", "lonely", "indifferent", "content", "peaceful", "happy", "joyful", "grateful", "calm", "energized", "excited", "amazed"];
 
+const touchedInputs = { mood: false, details: false, date: false };
+
 function getInputs() {
     const mood = document.getElementById('mood').value;
     const details = document.getElementById('details').value;
@@ -24,14 +26,22 @@ function isValidDateFormat(dateString) {
 }
 
 function displayErrors(mood, details, date) {
-    const errors = [
-        !valid_moods.includes(mood) ? `<p>${mood} is not a valid mood.</p>` : '',
-        typeof details !== 'string' || details.length > 500 ? "<p>Details must be text and less than 500 characters.</p>" : '',
-        !isValidDateFormat(date) ? "<p>Date must be YYYY-MM-DD.</p>" : new Date(date) > new Date() ? "<p>Date cannot be in the future.</p>" : ''
-    ].join('');
     const errorContainer = document.getElementById('error-container');
+    const errors = [
+        touchedInputs.mood && !valid_moods.includes(mood) && `<p>${mood} is not a valid mood.</p>`,
+        touchedInputs.details && (typeof details !== 'string' || details.length > 500) && '<p>Details must be text and less than 500 characters.</p>',
+        touchedInputs.date && (!isValidDateFormat(date) ? '<p>Date must be in the format YYYY-MM-DD.</p>' :
+            new Date(date) > new Date() && '<p>Date cannot be in the future.</p>')
+    ].filter(Boolean).join('');
     errorContainer.innerHTML = errors;
-    errorContainer.classList.toggle("w3-hide", !errors);
+    errorContainer.classList.toggle('w3-hide', !errors);
+}
+
+function handleInputTouch(event) {
+    const inputId = event.target.id;
+    if (touchedInputs.hasOwnProperty(inputId)) {
+        touchedInputs[inputId] = true;
+    }
 }
 
 function saveMood(mood, details, date) {
@@ -50,7 +60,6 @@ function saveMood(mood, details, date) {
         let response = parseJsonHelper(xhr.responseText);
         if (xhr.status == 200 && response.success == true) {
             fetchMoods();
-            resetForm();
         } else {
             let output = document.getElementById('error-container');
             document.getElementById('error-container').classList.remove("w3-hide");
@@ -66,8 +75,8 @@ function saveMood(mood, details, date) {
 
 function fetchMoods(category = 'all') {
     let xhr = new XMLHttpRequest();
-    let errorContainer = document.getElementById('error-container');
-    let outputContainer = document.getElementById('output');
+    const errorContainer = document.getElementById('error-container');
+    const outputContainer = document.getElementById('output');
     const handleError = (message) => {
         errorContainer.innerText = message;
         errorContainer.classList.remove("w3-hide");
@@ -78,32 +87,31 @@ function fetchMoods(category = 'all') {
         errorContainer.classList.add("w3-hide");
         outputContainer.innerHTML = "";
         if (xhr.status === 200) {
-            let response = JSON.parse(xhr.responseText);
+            const response = JSON.parse(xhr.responseText);
             if (response.success && response.moods) {
-                let moods = response.moods;
+                const moods = response.moods;
                 moods.sort((a, b) => new Date(b.date) - new Date(a.date));
                 const formatDate = (dateString) => {
-                    let date = new Date(dateString);
-                    let day = String(date.getDate()).padStart(2, '0');
-                    let month = String(date.getMonth() + 1).padStart(2, '0');
-                    let year = date.getFullYear();
+                    const date = new Date(dateString);
+                    const day = String(date.getDate()).padStart(2, '0');
+                    const month = String(date.getMonth() + 1).padStart(2, '0');
+                    const year = date.getFullYear();
                     return `${day}.${month}.${year}`;
                 };
                 moods.forEach((item) => {
-                    let moodEntry = document.createElement('div');
+                    const moodEntry = document.createElement('div');
                     moodEntry.classList.add('w3-card', 'w3-margin', 'w3-padding');
                     moodEntry.innerHTML = `
                         <h3>Mood: ${item.mood}</h3>
                         <p><strong>Date:</strong> ${formatDate(item.date)}</p>
-                        ${item.details? `<p><strong>Details:</strong> ${item.details}</p>`: ''}`;
+                        ${item.details ? `<p><strong>Details:</strong> ${item.details}</p>` : ''}`;
                     outputContainer.appendChild(moodEntry);
                 });
                 if (response.currentMoodCategory) {
                     drawSmiley(response.currentMoodCategory);
-                    
                     document.getElementById("currentMood").innerHTML = "Your Current Mood: " + response.currentMoodCategory;
                 }
-            } 
+            }
         } else {
             handleError("Failed to fetch moods.");
         }
@@ -117,28 +125,6 @@ function fetchMoods(category = 'all') {
     xhr.send();
 }
 
-function resetForm() {
-    const form = document.getElementById('moodForm');
-    form.reset();
-    const errorContainer = document.getElementById('error-container');
-    errorContainer.classList.add("w3-hide");
-    errorContainer.innerHTML = "";
-}
-
-document.getElementById('moodForm').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const mood_object = getInputs();
-    const mood = mood_object.mood;
-    const details = mood_object.details;
-    const date = mood_object.date;
-    document.getElementById('error-container').classList.add("w3-hide");
-    if (validate_inputs(mood, details, date)) {
-        saveMood(mood, details, date);
-    } else {
-        displayErrors(mood, details, date);
-    }
-});
-
 function parseJsonHelper(text) {
     try {
         return JSON.parse(text);
@@ -146,3 +132,20 @@ function parseJsonHelper(text) {
         return null;
     }
 }
+
+document.getElementById('moodForm').addEventListener('focus', handleInputTouch, true);
+document.getElementById('moodForm').addEventListener('blur', handleInputTouch, true);
+document.getElementById('moodForm').addEventListener('change', (event) => {
+    event.preventDefault();
+    const { mood, details, date } = getInputs();
+    displayErrors(mood, details, date);
+});
+document.getElementById('moodForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { mood, details, date } = getInputs();
+    if (validate_inputs(mood, details, date)) {
+        saveMood(mood, details, date);
+    } else {
+        displayErrors(mood, details, date);
+    }
+});
